bdf1fa4fdba8b6e7e82d64bee49ef48423f24044
JunHwan Kim <nefertiri972@gmail.com>
2022-05-16 00:59:21 +0900

    Add asynchronous validators
    - 비동기 유효성 검사기 생성 및 등록
    - 비동기 유효성 검사기는 Promise 또는 Observable 객체를 반환해야 함

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
       'userData': new FormGroup({
         // forbiddenNames가 호출되는 시점에 객체 내부에서 호출하는 것이 아니라, Angular가 호출하므로 bind 해야함
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -42,5 +43,18 @@ export class AppComponent implements OnInit {
       return {'nameIsForbidden': true};
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
